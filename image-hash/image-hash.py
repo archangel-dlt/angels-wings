6a26c64d1a997f353531df409728587ba94a0a7b
Jez Higgins <jez@jezuk.co.uk>
2019-08-02 07:36:24 +0100

    image-hasher placeholder

---
+++ image-hash/image-hash.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#from utils.extractor import Extractor
+
+if __name__ == '__main__':
+    #hasher = Extractor()
+
+    #img_path = 'cat.jpg'
+    #hash = hasher.extract(img_path)
+    println("[0.48221883 0.5587628  0.48636276 ... 0.         0.11017019 0.02501451]")
