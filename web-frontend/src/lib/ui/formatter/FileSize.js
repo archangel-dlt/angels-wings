e1807a01bb1c4a2891f8e31ab1d1f9cfbdc4fb21
Jez Higgins <jez@jezuk.co.uk>
2019-08-11 07:46:36 +0100

    removed unused code

--- web-frontend/src/lib/ui/formatter/FileSize.js
+++
@@ -1,7 +0,0 @@
-import prettysize from './prettysize';
-
-function FileSizeFormatter({value}) {
-  return prettysize(value);
-} // class FileSizeFormatter
-
-export default FileSizeFormatter;
--- web-frontend/src/lib/ui/formatter/Puid.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Puid({value}) {
-  return (
-    <a href={`http://www.nationalarchives.gov.uk/pronom/${value}`} target="_blank">{value}</a>
-  )
-}
-
-export default Puid;
