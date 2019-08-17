1fadd4114ba453c151acf6014cac5637a814afe7
Jez Higgins <jez@jezuk.co.uk>
2019-08-17 12:53:45 +0100

    Tweak how we overlay images so we don't break so many news sites

--- chrome-extension/scripts/content.js
+++ chrome-extension/scripts/content.js
@@ -2,8 +2,10 @@ const baseProtocol = window.location.protocol;
 const baseUrl = window.location.origin;
 const baseLocation = baseUrl + window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/')+1);
 
-const Authenticated = "<div class='archangel-authenticated'></div>";
-const NotAuthenticated = "<div class='archangel-not-authenticated'></div>";
+const AuthenticatedClass = 'archangel-authenticated';
+const NotAuthenticatedClass = 'archangel-not-authenticated';
+const AuthenticatedWrapper = `<div class='${AuthenticatedClass}'></div>`;
+const NotAuthenticatedWrapper = `<div class='${NotAuthenticatedClass}'></div>`
 
 angelsWings();
 
@@ -33,9 +35,21 @@ function markImage(image, isAuthentic) {
   if (isAuthentic == 'error')
     return;
 
+  // where the image is wrapped in a div and is the sole child,
+  // modify the div style directly - works best on a lot of news sites
+  const parent = image.element.parent();
+  if ((parent.children().length == 1) &&
+      (parent.is("div"))) {
+    parent.addClass(isAuthentic
+      ? AuthenticatedClass
+      : NotAuthenticatedClass
+    );
+    return;
+  }
+
   image.element.wrap(isAuthentic
-    ? Authenticated
-    : NotAuthenticated
+    ? AuthenticatedWrapper
+    : NotAuthenticatedWrapper
   );
 }
 
