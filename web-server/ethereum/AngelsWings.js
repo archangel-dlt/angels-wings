3574360014fd720aba8536ae8df46088c34bcde8
Jez Higgins <jez@jezuk.co.uk>
2019-08-24 10:48:26 +0100

    Save fingerprints to disk

--- web-server/ethereum/AngelsWings.js
+++ web-server/ethereum/AngelsWings.js
@@ -1,12 +1,31 @@
 import ArchangelEthereumDriver from './driver/ArchangelEthereumDriver';
-const ArchangelNetworkUrl = 'https://blockchain.surrey.ac.uk/ethereum';
+import path from 'path';
+import fs from 'fs';
+const fsp = fs.promises;
 
+const ArchangelNetworkUrl = 'https://blockchain.surrey.ac.uk/ethereum';
 let angelsWings = null;
+const fingerprintPath = path.join('/tmp', 'fingerprints');
+
+function setupDirectory() {
+  if (!fs.existsSync(fingerprintPath))
+    fs.mkdirSync(fingerprintPath);
+}
+
+function saveFingerprint(payload) {
+  const fileName = `photo-${payload.blockNumber}-${payload.txIndex}.json`;
+  const filePath = path.join(fingerprintPath, fileName);
+
+  fsp.writeFile(filePath, JSON.stringify(payload));
+  console.log(`Written ${fileName}`);
+}
 
 function StartAngelsWings() {
+  setupDirectory();
+
   angelsWings = new ArchangelEthereumDriver(
     ArchangelNetworkUrl,
-    console.log
+    saveFingerprint
   );
 }
 
