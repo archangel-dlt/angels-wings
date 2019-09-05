3befe91c14a26065f2ea204b8e22f433124dd0d7
Jez Higgins <jez@jezuk.co.uk>
2019-09-05 14:18:59 +0100

    Add simple lru cache of URLs into authenticate action

--- web-server/actions/authenticate-action.js
+++ web-server/actions/authenticate-action.js
@@ -1,20 +1,30 @@
 import fs from 'fs';
 import axios from 'axios';
 import path from 'path';
-import  os from 'os';
+import os from 'os';
+import LRU from 'lru-cache';
 import { getTempFilename as tempFilename } from 'express-fileupload/lib/utilities';
 import { authenticatePhoto } from '../imagehash/authenticate-photo'
 
+const cache = new LRU(500);
+
 async function authenticate(req, res) {
   const imageUrl = req.query.url;
+
   if (!imageUrl)
     return res.status(400).send('No file uploaded');
+
+  const imageInfo = cache.get(imageUrl);
+  if (imageInfo)
+    return res.json(imageInfo);
 
   let imagePath = null;
   try {
     imagePath = await downloadImage(imageUrl);
 
     const authInfo = await authenticatePhoto(imagePath);
+
+    cache.set(imageUrl, authInfo);
 
     res.json(authInfo);
   } catch (e) {
