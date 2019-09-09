cddcd52ae888d299f61f95c7eb22442898f828a8
Jez Higgins <jez@jezuk.co.uk>
2019-09-09 15:03:58 +0100

    submit urls up to 3 at a time so we don't murder the server

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,5 +1,8 @@
 const awURL = "http://localhost:3000/authenticate";
 
+let inflight = 0;
+const queued = [];
+
 chrome.runtime.onMessage.addListener(
   (imageUrl, sender, sendResponse) => {
     authenticateImage(imageUrl, sendResponse);
@@ -7,7 +10,31 @@ chrome.runtime.onMessage.addListener(
   }
 )
 
-async function authenticateImage(imageUrl, sendResponse) {
+function authenticateImage(imageUrl, sendResponse) {
+  ++inflight;
+  sendResponse = shimResponse(sendResponse)
+  if (inflight > 3) {
+    queued.push([imageUrl, sendResponse]);
+    return;
+  }
+
+  doAuthenticateImage(imageUrl, sendResponse);
+}
+
+function shimResponse(sendResponse) {
+  return data => {
+    sendResponse(data);
+
+    if (queued.length != 0) {
+      const [imageUrl, sendResponse] = queued.shift();
+      doAuthenticateImage(imageUrl, sendResponse);
+    }
+    --inflight;
+  }
+}
+
+
+async function doAuthenticateImage(imageUrl, sendResponse) {
   try {
     const response = await fetch(`${awURL}?url=${imageUrl}`);
     const data = await response.json();
